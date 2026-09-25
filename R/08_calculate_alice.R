5ee9e6a530a031c2896a4926aff79b79c04f00e2
TanyaDreiz <tdreizin@hawaii.edu>
2026-09-25 11:00:15 -1000

    Add rounding to ALICE hazard owner calculations

--- R/08_calculate_alice.R
+++ R/08_calculate_alice.R
@@ -1,4 +1,4 @@
-# Ordered from lowest to highest income bin
+#Ordered from lowest to highest income bin
 income_bins <- c(
   "less_10000", "10000_14999", "15000_19999", "20000_24999",
   "25000_29999", "30000_34999", "35000_39999", "40000_44999",
@@ -28,6 +28,36 @@ acs_clean <- acs_clean %>%
       avg_hh >= 4.5                     ~ alice5 / total_hh,
       TRUE ~ NA_real_
     )
+  )
+
+#Clean up alice and parcel count join dataframe
+clean_alice <- alice_pct_allcensus[, c("geoid", "tract", "block", "total_hh",
+                                       "owner", "alice_pct", "parcel_count")]
+
+#Create new column for percent of owners per CBG
+clean_alice$pct_owner <- clean_alice$owner / clean_alice$total_hh
+
+#Calculate count of homeowners that are in a CE-exposed parcel
+clean_alice$count_ce_owner <- clean_alice$parcel_count * clean_alice$pct_owner
+
+#Calculate count of homeowners exposed to CE that are ALICE
+clean_alice$count_ce_owner_alice <- clean_alice$count_ce_owner * clean_alice$alice_pct
+
+#Calculate percentage of ALICE homeowners per CBG
+clean_alice$pct_alice_hazard_owner <- (clean_alice$count_ce_owner_alice / clean_alice$count_ce_owner) * 100
+
+#double checking easier code
+alice_code_check <- alice_pct_allcensus %>%
+  mutate(
+    pct_owner = owner / total_hh,
+    total_ce_owner = parcel_count * pct_owner,
+    total_ce_owner_alice = total_ce_owner * alice_pct,
+    pct_alice_hazard_owner = ifelse(
+      total_ce_owner == 0,
+      NA,
+      round((total_ce_owner_alice / total_ce_owner) * 100, 1)
+    ),
+    pct_owner_display = round(pct_owner * 100, 1)
   )
 
 write.csv(acs_clean, here ("data", "processed", "census_data", "alice_pct_allcensus.csv"))
